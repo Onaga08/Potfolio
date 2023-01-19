120fe46eb0f422f9bae06736674931d3ad0386ee
Dhruv <pa8618@srmist.edu.in>
2023-01-19 16:01:43 +0530

    first

---
+++ index.js
@@ -0,0 +1,105 @@
+/*const btn = document.querySelector("#butt")
+const counter = document.getElementById('exception')
+const para = document.querySelector('.Paragraph')
+function button(){
+   
+    if(para.style.display == 'none'){
+        counter.style.height = "100px"
+    para.style.display = 'block'
+    }
+    else {
+        counter.style.height = '20px'
+        para.style.display = 'none'
+    }
+}
+
+btn.addEventListener('click', button)*/
+
+
+function increment1(){
+    
+    
+    if(document.querySelector(".child1").style.height === "100px"){
+    document.querySelector(".child1").style.height = "20px"
+    document.querySelector(".Paragraph1").style.display = "none"
+    document.querySelector(".child1").style.position =  null
+    document.querySelector(".child1").style.width = null
+    document.querySelector(".child1").style.top = null
+        }
+    
+      
+
+    else {
+        document.querySelector(".child1").style.height = "100px"
+        document.querySelector(".Paragraph1").style.display = "block"
+        document.querySelector(".child1").style.position = "sticky"
+        document.querySelector(".child1").style.width = "90%"
+        document.querySelector(".child1").style.top = "20px"
+    }
+}
+function increment2(){
+    
+    
+    if(document.querySelector(".child2").style.height === "100px"){
+    document.querySelector(".child2").style.height = "20px"
+    document.querySelector(".Paragraph2").style.display = "none"
+    document.querySelector(".child2").style.position =  null
+    document.querySelector(".child2").style.width = null
+    document.querySelector(".child2").style.top = null
+        }
+    
+      
+
+    else {
+        
+        document.querySelector(".child2").style.height = "100px"
+        document.querySelector(".Paragraph2").style.display = "block"
+        document.querySelector(".child2").style.position = "sticky"
+        document.querySelector(".child2").style.width = "90%"
+        document.querySelector(".child2").style.top = "20px"
+    }
+}
+function increment3(){
+    
+    
+    if(document.querySelector(".child3").style.height === "100px"){
+    document.querySelector(".child3").style.height = "20px"
+    document.querySelector(".Paragraph3").style.display = "none"
+    document.querySelector(".child3").style.position =  null
+    document.querySelector(".child3").style.width = null
+    document.querySelector(".child3").style.top = null
+        }
+    
+      
+
+    else {
+        
+        document.querySelector(".child3").style.height = "100px"
+        document.querySelector(".Paragraph3").style.display = "block"
+        document.querySelector(".child3").style.position = "sticky"
+        document.querySelector(".child3").style.width = "90%"
+        document.querySelector(".child3").style.top = "20px"
+    }
+}
+function increment4(){
+    
+    
+    if(document.querySelector(".child4").style.height === "100px"){
+    document.querySelector(".child4").style.height = "20px"
+    document.querySelector(".Paragraph4").style.display = "none"
+    document.querySelector(".child4").style.position =  null
+    document.querySelector(".child4").style.width = null
+    document.querySelector(".child4").style.top = null
+        }
+    
+      
+
+    else {
+        
+        document.querySelector(".child4").style.height = "100px"
+        document.querySelector(".Paragraph4").style.display = "block"
+        document.querySelector(".child4").style.position = "sticky"
+        document.querySelector(".child4").style.width = "90%"
+        document.querySelector(".child4").style.top = "20px"
+    }
+}
